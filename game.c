507a264f4eef2e3b6aea7571317688b054681df9
saveen-official <saveengimsara@gmail.com>
2026-06-21 06:31:57 +0530

    Updated code to add given functions

--- game.c
+++ game.c
@@ -2,32 +2,14 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main() {
-	srand(time(NULL));
+char map[15][15];
+int hiddenTraps[15][15];
 
-	char map[15][15];
-	int hiddenTraps[15][15];
-
-	// Setup exterior walls, interior empty spaces and Traps
-	for (int i = 0; i < 15; i++) {
-		for (int j = 0; j < 15; j++) {
-
-			if (i == 0 || i == 14 || j == 0 || j  == 14){
-				map[i][j] = '#';
-			}
-			else {
-				map[i][j] = ' ';
-			}
-
-			hiddenTraps[i][j] = 0;
-		}
-	}
-
-	// Placing 30 random interior walls
+//placing 30 random interior walls
+void placeWalls() {
 	int wallsPlaced = 0;
-
-        while (wallsPlaced < 30) {
-                int randomRow = (rand() % 13) + 1;
+	while (wallsPlaced < 30) {
+		int randomRow = (rand() % 13) + 1;
                 int randomCol = (rand() % 13) + 1;
 
                 if (map[randomRow][randomCol] == ' ') {
@@ -35,14 +17,40 @@ int main() {
                         wallsPlaced++;
                 }
         }
+}
 
-	// Printing the full map
+//print the full map
+void printMap() {
 	for (int i = 0; i < 15; i++) {
-		for (int j = 0; j < 15; j++) {
-			printf("%c", map[i][j]);
-		}
-		printf("\n");
-	}
+                for (int j = 0; j < 15; j++) {
+                        printf("%c", map[i][j]);
+                }
+                printf("\n");
+        }
+}
 
-return 0;
+// Setup exterior walls, interior empty spaces and Traps
+void initializeMap() {
+        for (int i = 0; i < 15; i++) {
+                for (int j = 0; j < 15; j++) {
+
+                        if (i == 0 || i == 14 || j == 0 || j  == 14){
+                                map[i][j] = '#';
+                        }
+                        else {
+                                map[i][j] = ' ';
+                        }
+
+                        hiddenTraps[i][j] = 0;
+                }
+        }
+	placeWalls();
+}
+
+int main() {
+	srand(time(NULL));
+	initializeMap();
+	printMap();
+
+	return 0;
 }
